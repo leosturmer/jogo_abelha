c442dc52629e4798301eea7c552d2c75659dce0c
leosturmer <you@example.com>
2026-03-05 12:03:49 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+
+// Configurando o Canvas
+
+const canvas = document.getElementById("canvas").getContext("2d");
+
+// Criando as instâncias dos objetos reais 
+
+var bee = new Bee(200, 500, 100, 100, "yellow");
+var spider = new Spider(100, 100, 100, 100, "black");
+
+// Os "escutadores" de eventos (Event Listeners)
+// Configurando teclas de navegabilidade
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "a") {
+        bee.dir = -5;
+    }
+    if (event.key === "d"){
+        bee.dir = +5;
+    }
+
+
+});
+
+document.addEventListener("keyup", function(event){
+    if (event.key === "a" || event.key === "d") {
+        bee.dir = 0;
+    };
+});
+
+
+
+
+// Funções básicas do projeto - Game loop
+
+// Função responsavel pela parte visual, tudo o que envolve desenho (Formas, imagens, textos, etc)
+
+function draw () {
+    bee.draw();
+    spider.draw();
+
+};
+
+// Função de lógica: cálculos, mudanças de posição de personagens, verificação de colisão
+
+function update () {
+
+    bee.move();
+    spider.move();
+
+};
+
+// Gerenciador, função que coordena o ciclo de vida de cada quadro (Frame)
+
+function main () {
+    canvas.clearRect(0,0, 1280,720);
+    update();
+    draw();
+
+};
+
+// Motor - Executar a função main a cada 10ms
+
+setInterval(main, 10);
+
+// requestAnimationFrame();
+
+
+
+
+
---
+++ js/util.js
@@ -0,0 +1,45 @@
+// Conterá a definição da forma. Não executa nada, apenas guarda a regra de como um objeto deve ser
+
+// POO
+
+class Obj {
+    constructor(x, y, width, height, color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    };
+
+    // Encapsulando 
+
+    draw () {
+        canvas.fillStyle = this.color;
+        canvas.fillRect(this.x, this.y, this.width, this.height);
+
+    };
+
+}
+
+class Bee extends Obj {
+    dir = 0;
+    
+    move () {
+        this.x += this.dir; // Movimento da abelha
+    };
+
+}
+
+class Spider extends Obj {
+
+    move(){
+        this.y += 4; // Move a arenha para baixo
+    
+
+        if (this.y > 900) {
+            this.y = -50;
+            this.x = Math.random() * (400 - 0);
+        };
+
+    }
+}
